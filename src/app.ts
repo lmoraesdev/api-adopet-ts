36bbe04e4c1707348a79144cbe5b2c737bce7f0d
lmoraesdev <lbc_moraes@outlook.com>
2025-02-11 17:31:32 -0300

    feat: criação da configuração  do banco de dados sqlite

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,17 @@
 import express, { Response } from 'express';
 import router from './routes';
+import 'reflect-metadata';
+import { AppDataSource } from './config/dataSource';
 
 const app = express();
 app.use(express.json());
 router(app);
 
-app.get('/', (_, res: Response) => {
-  res.send('Bem vindo ao curso de TypeScript!');
-});
-
-function criaPet(
-  id: Number,
-  nome: String,
-  especie: String,
-  idade: Number,
-  adotado: Boolean
-) {
-  return {
-    id,
-    nome,
-    especie,
-    idade,
-    adotado,
-  };
-}
-
-
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch((erro) => {
+    console.log('Error connecting to the database:', erro);
+  });
 export default app;
