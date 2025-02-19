52741266a0d66bd33dcfaf1c197cf3408a698fff
lmoraesdev <lbc_moraes@outlook.com>
2025-02-19 20:24:45 -0300

    feat: dto nos pets tbm

---
+++ src/types/tipoPets.ts
@@ -0,0 +1,17 @@
+import PetEntity from '../entities/PetEntity';
+import EnumEspecie from '../enum/EnumEspecie';
+
+type TipoRequestBodyPet = Omit<PetEntity, 'id'>;
+type TipoRequestParamsPet = {
+  id?: string;
+  pet_id?: string;
+  adotante_id?: string;
+};
+type TipoResponseBodyPet = {
+  data?:
+    | Pick<PetEntity, 'id' | 'nome' | 'especie' | 'porte'>
+    | Pick<PetEntity, 'id' | 'nome' | 'especie' | 'porte'>[];
+  error?: unknown;
+};
+
+export { TipoRequestBodyPet, TipoRequestParamsPet, TipoResponseBodyPet };
